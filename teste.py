a429aa48f6039654259450e00d3a5df5c102c6e4
Leo F <leonardoaguiarforner@gmail.com>
2024-08-22 17:22:27 -0300

    mais testes

--- teste.py
+++ teste.py
@@ -1,83 +1,145 @@
-import flet as ft
-from flet import (
-    Row,
-    FilePicker,
-    FilePickerResultEvent,
-    icons
-)
-from pathlib import Path
+# Importing Required Modules 
+from rembg import remove 
+import cv2
 
-def main(page: ft.Page):
-    page.title = "Visualizador de Imagens"
+# Store path of the image in the variable input_path 
+input_path = 'F:/backup de arquivos/fotos/IC/Nova pasta/teste3.png' 
+
+# Store path of the output image in the variable output_path 
+output_path = 'F:/backup de arquivos/fotos/IC/Nova pasta/teste3b2g.png' 
 
 
+input = cv2.imread(input_path)
+output = remove(input)
+cv2.imwrite(output_path, output)
 
-    # Open directory dialog
-    def get_directory_result(e: FilePickerResultEvent):
-        directory_path.value = e.path if e.path else "Cancelled!"     
-        exibir_imagens( directory_path.value)
-        directory_path.update()
+
+import cv2
+import numpy as np
+import math
+
+# input image
+path = "F:/backup de arquivos/fotos/IC/imagens-20240604T172236Z-001/Nova pasta/1jHSf.jpg"
+# 1 EUR coin diameter in cm
+coinDiameter = 2.325
+# real area for the coin in cm^2
+coinArea = (coinDiameter/2)**2 * math.pi
+# initializing the multiplying factor for real size
+realAreaPerPixel = 1
+
+
+# pixel to cm^2
+def pixelToArea(objectSizeInPixel, coinSizeInPixel):
+    # how many cm^2 per pixel?
+    realAreaPerPixel = coinArea / coinSizeInPixel
+    print("realAreaPerPixel: ", realAreaPerPixel)
+    # object area in cm^2
+    objectArea = realAreaPerPixel * objectSizeInPixel
+    return objectArea    
+
+
+# finding coin and steak contours
+def getContours(img, imgContour):
+    
+    # find all the contours from the B&W image
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # needed to filter only our contours of interest
+    finalContours = []
+    
+    # for each contour found
+    for cnt in contours:
+        # cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 2)
+        # find its area in pixel
+        area = cv2.contourArea(cnt)
+        print("Detected Contour with Area: ", area)
+
+        # minimum area value is to be fixed as the one that leaves the coin as the small object on the scene
+        if (area > 5000):
+            perimeter = cv2.arcLength(cnt, True)
+            
+            # smaller epsilon -> more vertices detected [= more precision]
+            epsilon = 0.002*perimeter
+            # check how many vertices         
+            approx = cv2.approxPolyDP(cnt, epsilon, True)
+            #print(len(approx))
+            
+            finalContours.append([len(approx), area, approx, cnt])
+
+    # we want only two objects here: the coin and the meat slice
+    print("---\nFinal number of External Contours: ", len(finalContours))
+    # so at this point finalContours should have only two elements
+    # sorting in ascending order depending on the area
+    finalContours = sorted(finalContours, key = lambda x:x[1], reverse=False)
+    
+    # drawing contours for the final objects
+    for con in finalContours:
+        cv2.drawContours(imgContour, con[3], -1, (0, 0, 255), 3)
+
+    return imgContour, finalContours
 
     
+# sourcing the input image
+img = cv2.imread(path)
+cv2.imshow("Starting image", img)
+cv2.waitKey()
+
+# blurring
+imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
+# graying
+imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+# canny
+imgCanny = cv2.Canny(imgGray, 255, 195)
+
+kernel = np.ones((2, 2))
+imgDil = cv2.dilate(imgCanny, kernel, iterations = 3)
+# cv2.imshow("Diluted", imgDil)
+imgThre = cv2.erode(imgDil, kernel, iterations = 3)
+
+imgFinalContours, finalContours = getContours(imgThre, img)
+
+# first final contour has the area of the coin in pixel
+coinPixelArea = finalContours[0][1]
+print("Coin Area in pixel", coinPixelArea)
+# second final contour has the area of the meat slice in pixel
+slicePixelArea = finalContours[1][1]
+print("Entire Slice Area in pixel", slicePixelArea)
+
+# let's go cm^2
+print("Coin Area in cm^2:", coinArea)
+print("Entire Slice Area in cm^2:", pixelToArea(slicePixelArea, coinPixelArea))
+
+# show  the contours on the unfiltered starting image
+cv2.imshow("Final External Contours", imgFinalContours)
+cv2.waitKey()
 
 
-    def exibir_imagens(pasta):
-        imagens.controls.clear()
-        for arquivo in Path(pasta).iterdir():
-            if arquivo.is_file() and arquivo.suffix.lower() in (".jpg", ".jpeg", ".png", ".gif"):
-                imagens.controls.append(
-                    ft.Image(
-                        src=str(arquivo),  # Caminho do arquivo,
-                        width=300,  # Largura da imagem
-                        height=200, # Altura da imagem
-                        fit=ft.ImageFit.FILL,  # Ajuste da imagem
-                        border_radius=5,
-                    )
-                )
-        page.update()
+# now let's detect and quantify the lean part
 
+# convert to HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
+# set lower and upper color limits
+lowerVal = np.array([0, 159, 160])
+upperVal = np.array([101, 255, 253])
+# Threshold the HSV image to get only red colors
+mask = cv2.inRange(hsv, lowerVal, upperVal)
+# apply mask to original image - this shows the red with black blackground
+final = cv2.bitwise_and(img, img, mask= mask)
 
-        # Pasta inicial (pode ser ajustada)
-    pasta_inicial = Path.home() / "OneDrive - UFSC/Imagens/Capturas de tela"
+# show selection
+cv2.imshow("Lean Cut", final)
+cv2.waitKey()
 
-    
-    imagens = ft.Column( scroll=ft.ScrollMode.ALWAYS, expand=True)
-    pasta_selecionada = ft.Text(f"Pasta selecionada: {pasta_inicial}")
-    get_directory_dialog = FilePicker(on_result=get_directory_result)
-    directory_path = ft.Text()
+# convert it to grayscale because countNonZero() wants 1 channel images
+gray = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
+# cv2.imshow("Gray", gray)
+# cv2.waitKey()
+meatyPixelArea = cv2.countNonZero(gray)
 
+print("Red Area in pixel: ", meatyPixelArea)
+print("Red Area in cm^2: ", pixelToArea(meatyPixelArea, coinPixelArea))
 
-    # hide all dialogs in overlay
-    page.overlay.extend([get_directory_dialog])
+# finally the body-fat ratio calculation
+print("Body-Fat Ratio: ", meatyPixelArea/slicePixelArea*100, "%")
 
-
-    page.add(
-        Row(
-        [
-            pasta_selecionada,
-            directory_path,
-            ft.ElevatedButton(
-                "Selecionar Pasta",
-                icon=icons.FOLDER_OPEN,
-                on_click=lambda _: get_directory_dialog.get_directory_path(),
-                disabled=page.web,
-            ),
-            imagens,
-        ]
-        ),
-        Row(
-        [
-            pasta_selecionada,
-            directory_path,
-            ft.ElevatedButton(
-                "Selecionar Pasta",
-                icon=icons.FOLDER_OPEN,
-                on_click=lambda _: get_directory_dialog.get_directory_path(),
-                disabled=page.web,
-            ),
-            imagens,
-        ]
-        ),
-    )
-
-ft.app(target=main)
+cv2.destroyAllWindows()
