d712340cfb06a03b569f8a3de8fb1f467ae1928b
Leo F <leonardoaguiarforner@gmail.com>
2024-08-27 20:15:10 -0300

    código para corte da imagem pronto

--- funcoes/corte_da_imagem.py
+++ funcoes/corte_da_imagem.py
@@ -1,25 +1,31 @@
 import cv2
+import os
+def crop(input_path, up, bottom, left, right):
+    # ler a imagem
+    image = cv2.imread(input_path)
 
-def corte(imagem,cima,baixo,esquerda,direita):
-    # obter dimensões da imagem
-    altura, largura = imagem.shape[:2]
-    corte_esquerda = largura*esquerda
-    corte_direita = largura-(largura*direita)
-    corte_cima = altura*cima
-    corte_baixo = altura-(altura*baixo)
+    # obter as dimensões da imagem, as variáveis recebidas são as porcentagens de corte
+    height, width = image.shape[:2]
+    left_slice = width * left
+    right_slice = width - (width * right)
+    top_slice = height * up
+    bottom_slice = height - (height * bottom)
 
     # cortar a imagem
-    imagem_cortada = imagem[int(corte_cima):int(corte_baixo), int(corte_esquerda):int(corte_direita)]
+    cropped_image = image[int(top_slice):int(bottom_slice), int(left_slice):int(right_slice)]
 
-    return imagem_cortada
+    # criar o diretório "imagens cortadas" a partir do diretório da imagem original
+    original_directory = os.path.dirname(input_path)
+    cropped_directory = os.path.join(original_directory, "imagens cortadas")
+    os.makedirs(cropped_directory, exist_ok=True)
 
-# Store path of the image in the variable input_path 
-input_path = 'F:/backup de arquivos/fotos/IC/Nova pasta/teste3.png' 
+    # salvar a imagem cortada no diretório
+    image_name = os.path.basename(input_path)
+    cropped_image_path = os.path.join(cropped_directory, image_name)
+    cv2.imwrite(cropped_image_path, cropped_image)
 
-# Store path of the output image in the variable output_path 
-output_path = 'F:/backup de arquivos/fotos/IC/Nova pasta/teste3b2g.png' 
+    return cropped_image_path
 
-
-input = cv2.imread(input_path)
-output = corte(input,0.2,0.2,0.2,0.2)#remove(input)
-cv2.imwrite(output_path, output) # type: ignore
+# Exemplo de uso
+input_path = 'imagens de exemplo/teste.tif'
+output = crop(input_path, 0.2, 0.2, 0.2, 0.2)
